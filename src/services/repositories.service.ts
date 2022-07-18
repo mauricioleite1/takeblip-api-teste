4b895f3bdcc43c06c6d24ea6ebf7e69be1b9d2bd
Maurício Leite <mauricioleite1@gmail.com>
2022-07-18 08:16:33 -0300

    feature: service filtrando e devolvendo apenas 5 repositórios

--- src/services/repositories.service.ts
+++ src/services/repositories.service.ts
@@ -1,24 +1,26 @@
 import repositoriesModel from "../models/repositories.model";
-import { IRepository } from "../utils/interfaces";
+import { IRepositoriesList } from "../utils/interfaces";
 
 const getAll = async () => {
   const data = await repositoriesModel.getAll();
   return data;
-}
+};
 
 const getByLanguage = async (language: string) => {
-  const data: IRepository[] = await repositoriesModel.getAll();
-  const filteredData: IRepository[] = data
-    .map(({ full_name, description, language }) => ({
+  const data: IRepositoriesList = await repositoriesModel.getAll();
+  const onlyFiveReposByLanguage: IRepositoriesList = data
+    .filter((a) => a.language === language)
+    .map(({ full_name, description, language, owner: { avatar_url } }) => ({
       full_name,
       description,
       language,
-
+      owner: {
+        avatar_url,
+      },
     }))
-    .filter((a) => a.language === language)
-    .slice(0, 5)
+    .slice(0, 5);
 
-  return filteredData;
-}
+  return onlyFiveReposByLanguage;
+};
 
 export default { getAll, getByLanguage };
