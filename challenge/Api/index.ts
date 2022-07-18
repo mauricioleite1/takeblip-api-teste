8f57799a3d77fd45383e2ea923617d753b608006
Maurício Leite <mauricioleite1@gmail.com>
2022-07-17 23:39:06 -0300

    refactor: params sem números limitantes de itens

--- challenge/Api/index.ts
+++ challenge/Api/index.ts
@@ -9,12 +9,11 @@ const org = process.env.ORG || 'takenet';
 const listAllRepositoriesEndpoint = `/orgs/${org}/repos`;
 
 const settings = {
-  quantity: 5,
   sortBy: 'created',
   direction: 'asc'
 }
 
-const params = `per_page=${settings.quantity}&sort=${settings.sortBy}&direction=${settings.direction}`;
+const params = `sort=${settings.sortBy}&direction=${settings.direction}`;
 
 export {
   githubApi,
