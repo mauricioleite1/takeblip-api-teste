8f8b891afdad2664d9973d6a79f8d3ab8cfdd28f
Maurício Leite <mauricioleite1@gmail.com>
2022-07-18 08:17:27 -0300

    feature: interfaces tipadas

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,5 +1,12 @@
+export interface IOwnersAvatar {
+  avatar_url: string;
+}
+
 export interface IRepository {
   full_name: string;
   description: string;
   language: string;
+  owner: IOwnersAvatar;
 }
+
+export interface IRepositoriesList extends Array<IRepository> {}
