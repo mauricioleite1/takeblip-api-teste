9f3207e5cb650b650ac6e778a44717d64ece0cc4
Maurício Leite <mauricioleite1@gmail.com>
2022-07-18 12:27:50 -0300

    refactor: revisão do app

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(router)
+app.use(router);
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
+
+export default app;
